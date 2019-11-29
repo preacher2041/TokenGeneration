40a7f3210709248acd9a9946b65dd7cd81ada067
pdc <pdc@pdcs-MacBook-Pro.local>
2019-11-29 11:12:25 +0000

    Add links to routing

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
 import React from 'react';
-import {Switch, Route} from 'react-router-dom';
+import {Switch, Route, NavLink} from 'react-router-dom';
 import PointOfSalePageView from './pages/PointOfSale';
 import ReportsPageView from './pages/Reports';
 
 // Renders the root component
 export const App = () => {
 	return (
-		<React.Fragment>
-			<Switch>
-			<Route exact path="/point-of-sale" render={props => <PointOfSalePageView {...props} />} />
-			<Route path="/reports" render={props => <ReportsPageView {...props} />} />
-		</Switch>
-		</React.Fragment>
+		<div>
+			<NavLink
+				to="/point-of-sale">
+				Point of Sale
+			</NavLink>
+			<NavLink
+				to="/reports">
+				Reports
+			</NavLink>
+			<React.Fragment>
+				<Switch>
+				<Route exact path="/point-of-sale" render={props => <PointOfSalePageView {...props} />} />
+				<Route path="/reports" render={props => <ReportsPageView {...props} />} />
+			</Switch>
+			</React.Fragment>
+		</div>
 	)
 };
