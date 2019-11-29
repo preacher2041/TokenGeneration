1ab33a820a7898269520b48851193abc52b92886
pdc <pdc@pdcs-MacBook-Pro.local>
2019-11-29 13:14:17 +0000

    Arrange cell

--- src/components/ResultsRow/ResultsRow.jsx
+++ src/components/ResultsRow/ResultsRow.jsx
@@ -18,11 +18,11 @@ const ResultsRow = ({rows, productData}) => {
 			<Table className={styles.formControl} aria-label="simple table">
 				<TableHead>
 					<TableRow>
-						<TableCell>Product Name</TableCell>
-						<TableCell>Voucher type ID</TableCell>
-						<TableCell align="right">Retailer ID</TableCell>
-						<TableCell align="right">Delivered</TableCell>
-						<TableCell align="right">Not delivered</TableCell>
+						<TableCell align="center">Product Name</TableCell>
+						<TableCell align="center">Voucher type ID</TableCell>
+						<TableCell align="center">Retailer ID</TableCell>
+						<TableCell align="center">Delivered</TableCell>
+						<TableCell align="center">Not delivered</TableCell>
 					</TableRow>
 				</TableHead>
 				<TableBody>
@@ -31,17 +31,17 @@ const ResultsRow = ({rows, productData}) => {
 							{productData.map((product, i) => {
 								if(productData[i].typeID == row.voucherTypeId){
 								return (
-									<TableCell component="th" scope="row" key={i}>
+									<TableCell align="center" component="th" scope="row" key={i}>
 										{productData[i].typeID == row.voucherTypeId ? product.productName : ''}
 									</TableCell>
 								)} else {
 									return;
 								}
 							})}
-							<TableCell align="right">{row.voucherTypeId}</TableCell>
-							<TableCell align="right">{row.retailerId}</TableCell>
-							<TableCell align="right">{row.count - row.countNotDelivered}</TableCell>
-							<TableCell align="right">{row.countNotDelivered}</TableCell>
+							<TableCell align="center">{row.voucherTypeId}</TableCell>
+							<TableCell align="center">{row.retailerId}</TableCell>
+							<TableCell align="center">{row.count - row.countNotDelivered}</TableCell>
+							<TableCell align="center">{row.countNotDelivered}</TableCell>
 						</TableRow>
 					))}
 				</TableBody>
